28fe3b8df0c40f3f0a86766429d0f76b6410fdd6
= <=>
2024-09-05 02:58:24 -0700

    recover main

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <thread>
+#include "remote_server.h"
+
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+        RemoteServer server(io_context, 12345);
+
+        std::thread server_thread([&io_context]()
+                                  { io_context.run(); });
+        server_thread.join();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
